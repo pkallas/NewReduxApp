bca0a653685ae8c4cd1dc2419710c84076328b71
pkallas <pkallas@Patricks-MacBook-Pro.local>
2017-10-25 15:49:11 -0700

    Readded google maps script, changed Kelvin to Fahrenheit, made new graphs appear at top of page

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
-    const temps = cityData.list.map(weather => weather.main.temp);
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), newTemp => (newTemp * 9/5) - 459.67);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
     return (
       <tr key={cityData.city.id}>
         <td>{cityData.city.name}</td>
-        <td><Chart data={temps} color="cyan" units="K" /></td>
+        <td><Chart data={temps} color="cyan" units="F" /></td>
         <td><Chart data={pressures} color="green" units="hPa" /></td>
         <td><Chart data={humidities} color="gold" units="%" /></td>
       </tr>
@@ -23,7 +24,7 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature (K)</th>
+            <th>Temperature (F)</th>
             <th>Pressure (hPa)</th>
             <th>Humidity (%)</th>
           </tr>
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -2,7 +2,8 @@ import { FETCH_WEATHER } from '../actions/index';
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_WEATHER:
-      return state.concat([action.payload.data]);
+      return [action.payload.data, ...state];
+    default:
+      return state;
   }
-  return state;
 };
