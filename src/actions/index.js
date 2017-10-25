a6ebc1b1f198e42b528d54e39a0d5eb64dfbb7e6
pkallas <pkallas@Patricks-MacBook-Pro.local>
2017-10-24 17:57:07 -0700

    Lots of changes

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+const API_KEY = 'b51b641219666b974b540f2b14ac7871';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+import axios from 'axios';
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  };
+};
